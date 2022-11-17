8c266754ee516f29d9d5ecf67740a4dac5860342
AfnanSiregar <francafnan@gmail.com>
2022-11-17 13:58:08 +0700

    update

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -8,7 +8,7 @@ function HeroSection() {
           <div className='text-center'>
             <h1 className='text-2xl md:text-4xl mb-1 md:mb-3 text-indigo-600 fonts-semibold dark:text-indigo-500'>Hi, I'm Afnan. <br></br> A Programmer.</h1>
             <p className='text-md md:text-xl max-w-md mb-3 text-gray-600 dark:text-gray-300'>I'm a front-end and back-end developer with high curiosity in technology</p>
-            <a href='/CV-Muzayyin Al-Afnan Siregar.pdf' target="_blank" spy={true} smooth={true} offset={-100} duration={500} className='inline-block px-8 py-3 border border-tranparent text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 md:text-md'>See My CV</a>
+            <a href='/CV-Muzayyin Al-Afnan Siregar.pdf' target="_blank" className='inline-block px-8 py-3 border border-tranparent text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 md:text-md'>See My CV</a>
           </div>
         </div>
       </section>
